fdd63284ab893cf392221e881a1102e948c2dbef
aljarrah0 <moh.moh.aljarrah@gmail.com>
2025-03-17 00:44:32 +0200

    Add PostController.php

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class PostController extends Controller
+{
+    function index()
+    {
+        $posts = [
+            ['id' => 1, 'title' => 'HTML', 'posted_by' => 'Mohammed', 'created_at' => '2025-01-011'],
+            ['id' => 1, 'title' => 'CSS', 'posted_by' => 'Ahmed', 'created_at' => '2025-01-10'],
+            ['id' => 1, 'title' => 'JS', 'posted_by' => 'Amjad', 'created_at' => '2025-01-09'],
+            ['id' => 1, 'title' => 'PHP', 'posted_by' => 'Yasin', 'created_at' => '2025-01-01'],
+        ];
+        return view('posts.index', ['posts' => $posts]);
+    }
+}
