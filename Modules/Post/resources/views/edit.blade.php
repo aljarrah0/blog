da9c666a4da85d8aa0f5f35cfc197bc8e47da8e9
aljarrah0 <moh.moh.aljarrah@gmail.com>
2025-03-26 16:37:33 +0200

    Add validation and old input retention to post edit form fields

--- Modules/Post/resources/views/edit.blade.php
+++ Modules/Post/resources/views/edit.blade.php
@@ -14,18 +14,18 @@
         @method('PUT')
         <div class="mb-3">
             <label for="title" class="form-label">title</label>
-            <input type="text" name="title" class="form-control" id="title" placeholder="title input placeholder" value="{{ $post->title }}">
+            <input type="text" name="title" class="form-control" id="title" placeholder="title input placeholder" required value="{{ old('title', $post->title) }}">
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">description</label>
-            <textarea class="form-control" name="description" placeholder="description input placeholder">{{ $post->description }}</textarea>
+            <textarea class="form-control" name="description" placeholder="description input placeholder" required>{{ old('description', $post->description) }}</textarea>
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Posted By</label>
-            <select class="form-select" name="user_id" aria-label="Default select example">
-                <option selected>Open this select menu</option>
+            <select class="form-select" name="user_id" aria-label="Default select example" required>
+                <option value="" selected>Open this select menu</option>
                 @foreach($users as $user)
-                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                    <option @selected(old('user_id', $post->user_id) == $user->id) value="{{ $user->id }}">{{ $user->name }}</option>
                 @endforeach
             </select>
         </div>
