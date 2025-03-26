9742b4b22ee67e10dc0a282ab9038e2aae4a825f
aljarrah0 <moh.moh.aljarrah@gmail.com>
2025-03-26 13:46:34 +0200

    Preserve form input values on create post page

--- Modules/Post/resources/views/create.blade.php
+++ Modules/Post/resources/views/create.blade.php
@@ -4,18 +4,20 @@
         @csrf
         <div class="mb-3">
             <label for="title" class="form-label">title</label>
-            <input type="text" name="title" class="form-control" id="title" placeholder="title input placeholder" required minlength="3">
+            <input type="text" name="title" class="form-control" id="title" placeholder="title input placeholder"
+                   required minlength="3" value="{{ old('title') }}">
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">description</label>
-            <textarea class="form-control" name="description" placeholder="description input placeholder" required minlength="3"></textarea>
+            <textarea class="form-control" name="description" placeholder="description input placeholder" required
+                      minlength="3">{{ old('description') }}</textarea>
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Posted By</label>
             <select class="form-select" name="user_id" aria-label="Default select example" required>
                 <option value="" selected>Open this select menu</option>
                 @foreach($users as $user)
-                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                    <option @selected(old('user_id') == $user->id) value="{{ $user->id }}">{{ $user->name }}</option>
                 @endforeach
             </select>
         </div>
