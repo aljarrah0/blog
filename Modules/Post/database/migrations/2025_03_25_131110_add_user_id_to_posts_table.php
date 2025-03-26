1f9e1abb3f0a1073728ab716409b79db0b4521fe
aljarrah0 <moh.moh.aljarrah@gmail.com>
2025-03-26 09:10:22 +0200

    Make user_id nullable in posts table migration

--- Modules/Post/database/migrations/2025_03_25_131110_add_user_id_to_posts_table.php
+++ Modules/Post/database/migrations/2025_03_25_131110_add_user_id_to_posts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('user_id')->nullable();
             $table->foreign('user_id')->references('id')->on('users');
         });
     }
