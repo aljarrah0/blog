9f85f1a3b949ba611fe8a5ae968addf27bf24995
aljarrah0 <moh.moh.aljarrah@gmail.com>
2025-03-26 13:37:18 +0200

    Add validation requirements to create post form fields

--- Modules/Post/resources/views/create.blade.php
+++ Modules/Post/resources/views/create.blade.php
@@ -4,15 +4,15 @@
         @csrf
         <div class="mb-3">
             <label for="title" class="form-label">title</label>
-            <input type="text" name="title" class="form-control" id="title" placeholder="title input placeholder">
+            <input type="text" name="title" class="form-control" id="title" placeholder="title input placeholder" required minlength="3">
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">description</label>
-            <textarea class="form-control" name="description" placeholder="description input placeholder"></textarea>
+            <textarea class="form-control" name="description" placeholder="description input placeholder" required minlength="3"></textarea>
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Posted By</label>
-            <select class="form-select" name="user_id" aria-label="Default select example">
+            <select class="form-select" name="user_id" aria-label="Default select example" required>
                 <option value="" selected>Open this select menu</option>
                 @foreach($users as $user)
                     <option value="{{ $user->id }}">{{ $user->name }}</option>
