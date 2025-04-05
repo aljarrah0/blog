0ab75e2bf463c778e7bc4e78a0bbc51d9e626e67
aljarrah0 <moh.moh.aljarrah@gmail.com>
2025-04-05 19:29:54 +0200

    Add validation feedback for form inputs in create and edit views

--- Modules/Post/resources/views/create.blade.php
+++ Modules/Post/resources/views/create.blade.php
@@ -1,37 +1,57 @@
 @extends('post::layouts.master')
 @section('content')
+    @php
+        $isSubmitted = old('_token') !== null;
+    @endphp
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <form method="POST" action="{{ route('posts.store') }}">
         @csrf
         <div class="mb-3">
             <label for="title" class="form-label">title</label>
-            <input type="text" name="title" class="form-control @error('title') is-invalid @else is-valid @enderror" id="title"
+            <input type="text" name="title"
+                   class="form-control
+                    @if($isSubmitted)
+                        @error('title') is-invalid @else is-valid @enderror
+                    @endif"
+                   id="title"
                    placeholder="title input placeholder"
                    required minlength="3" value="{{ old('title') }}"
             >
-            @error('title')
-            <div class="alert alert-danger mt-2">{{ $message }}</div>
-            @enderror
+
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">description</label>
-            <textarea class="form-control @error('description') is-invalid @else is-valid @enderror" name="description"
-                      placeholder="description input placeholder" required
-                      minlength="3">{{ old('description') }}</textarea>
-            @error('description')
-            <div class="alert alert-danger mt-2">{{ $message }}</div>
-            @enderror
+            <textarea
+                    class="form-control
+                    @if($isSubmitted)
+                        @error('description') is-invalid @else is-valid @enderror
+                    @endif"
+                    name="description"
+                    placeholder="description input placeholder" required
+                    minlength="3">{{ old('description') }}</textarea>
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Posted By</label>
-            <select class="form-select @error('user_id') is-invalid @else is-valid @enderror" name="user_id" aria-label="Default select example">
+            <select
+                    class="form-control
+                    @if($isSubmitted)
+                        @error('user_id') is-invalid @else is-valid @enderror
+                    @endif"
+                    name="user_id"
+                    aria-label="Default select example">
                 <option value="" selected>Open this select menu</option>
                 @foreach($users as $user)
                     <option @selected(old('user_id') == $user->id) value="{{ $user->id }}">{{ $user->name }}</option>
                 @endforeach
             </select>
-            @error('user_id')
-            <div class="alert alert-danger mt-2">{{ $message }}</div>
-            @enderror
         </div>
         <button type="submit" class="btn btn-success">Create</button>
     </form>
--- Modules/Post/resources/views/edit.blade.php
+++ Modules/Post/resources/views/edit.blade.php
@@ -1,5 +1,8 @@
 @extends('post::layouts.master')
 @section('content')
+    @php
+        $isSubmitted = old('_token') !== null;
+    @endphp
     @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
@@ -14,15 +17,27 @@
         @method('PUT')
         <div class="mb-3">
             <label for="title" class="form-label">title</label>
-            <input type="text" name="title" class="form-control" id="title" placeholder="title input placeholder" required value="{{ old('title', $post->title) }}">
+            <input type="text" name="title" class="form-control
+                    @if($isSubmitted)
+                        @error('title') is-invalid @else is-valid @enderror
+                    @endif" id="title" placeholder="title input placeholder" required
+                   value="{{ old('title', $post->title) }}">
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">description</label>
-            <textarea class="form-control" name="description" placeholder="description input placeholder" required>{{ old('description', $post->description) }}</textarea>
+            <textarea class="form-control
+                    @if($isSubmitted)
+                        @error('description') is-invalid @else is-valid @enderror
+                    @endif" name="description" placeholder="description input placeholder"
+                      required>{{ old('description', $post->description) }}</textarea>
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Posted By</label>
-            <select class="form-select" name="user_id" aria-label="Default select example" required>
+            <select
+                    class="form-select
+                    @if($isSubmitted)
+                        @error('user_id') is-invalid @else is-valid @enderror
+                    @endif" name="user_id" aria-label="Default select example" required>
                 <option value="" selected>Open this select menu</option>
                 @foreach($users as $user)
                     <option @selected(old('user_id', $post->user_id) == $user->id) value="{{ $user->id }}">{{ $user->name }}</option>
