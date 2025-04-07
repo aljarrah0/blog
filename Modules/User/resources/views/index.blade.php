9440c0d45ed5e94f56f08889ece1b21f383ee95b
aljarrah0 <moh.moh.aljarrah@gmail.com>
2025-04-07 03:04:53 +0200

    Add success message display in index view

--- Modules/User/resources/views/index.blade.php
+++ Modules/User/resources/views/index.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 @section('content')
+    @if(session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
     <div class="text-center">
         <a href="{{ route('users.create') }}" class="btn btn-success">Create user</a>
         <table class="table mt-4">
