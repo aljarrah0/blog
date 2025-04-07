c995df55b32195eaf9168ad10d0139907beb66a0
aljarrah0 <moh.moh.aljarrah@gmail.com>
2025-04-07 03:04:46 +0200

    Add success message display for user actions in show view

--- Modules/User/resources/views/show.blade.php
+++ Modules/User/resources/views/show.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 @section('content')
+    @if(session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
     <div class="text-center">
         <div class="card">
             <div class="card-header">
